cf436e847537c1d14e5e5b40c129cd8f3de48937
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-04-10 07:07:26 +0800

    init project, simplejwt auth, postgre

---
+++ apps/authentication/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
+urlpatterns = [
+    path("api/login/", TokenObtainPairView.as_view(), name="login"),
+    path("api/login/refresh/", TokenRefreshView.as_view(), name="login_refresh"),
+]
