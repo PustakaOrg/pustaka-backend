8239b9d301bc7897c8f5c7cc95ff98b2b75f5ab8
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-04-12 06:14:59 +0800

    initilal catalog models and serilizers

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -2,20 +2,44 @@ from django.db import models
 
 from core.models import BaseModel
 
-class Book(BaseModel):
-    pass
-
-
-class Category(BaseModel):
-    pass
-
-
-class Shelf(BaseModel):
-    pass
 
 class Author(BaseModel):
-    pass
+    fullname = models.CharField(max_length=50)
 
 
 class Publisher(BaseModel):
-    pass
+    name = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=20)
+
+
+class Shelf(BaseModel):
+    code = models.CharField(max_length=10)
+
+
+class Book(BaseModel):
+    title = models.CharField(max_length=100, null=False, blank=False, db_index=True)
+    isbn = models.CharField(
+        max_length=13, null=False, blank=False, unique=True, db_index=True
+    )
+    pages = models.PositiveSmallIntegerField(default=0)
+    publish_year = models.PositiveSmallIntegerField(default=0)
+    stock = models.PositiveSmallIntegerField(default=1)
+    available_stock = models.PositiveSmallIntegerField(default=1)
+    shelf = models.ForeignKey(to=Shelf, on_delete=models.SET_NULL, null=True, related_name="books")
+    category = models.ManyToManyField(to=Category, related_name="related_books")
+    author = models.ForeignKey(to=Author, on_delete=models.SET_NULL, null=True, related_name="books")
+    publisher = models.ForeignKey(to=Publisher, on_delete=models.SET_NULL, null=True, related_name="books")
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.available_stock = self.stock
+        super().save(*args, **kwargs)
+
+
+
+
+
---
+++ apps/catalog/serializer.py
@@ -0,0 +1,94 @@
+from rest_framework import serializers
+from .models import Author, Publisher, Category, Shelf, Book
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ["fullname"]
+
+
+class PublisherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publisher
+        fields = ["name", "city"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["name"]
+
+
+class ShelfSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shelf
+        fields = ["code"]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)  
+    publisher = PublisherSerializer(read_only=True)  
+    category = CategorySerializer(many=True, read_only=True)  
+
+    def create(self, validated_data):
+        author_data = validated_data.pop('author', None)
+        publisher_data = validated_data.pop('publisher', None)
+        category_data = validated_data.pop('category', [])
+        author = None
+        publisher = None
+
+        if author_data:
+            author, created = Author.objects.get_or_create(**author_data)
+
+        if publisher_data:
+            publisher, created = Publisher.objects.get_or_create(**publisher_data)
+
+        book = Book.objects.create(author=author, publisher=publisher, **validated_data)
+
+        for category in category_data:
+            category_obj, created = Category.objects.get_or_create(**category)
+
+            book.category.add(category_obj)
+
+        return book
+
+    def update(self, instance, validated_data):
+        author_data = validated_data.pop('author', None)
+        publisher_data = validated_data.pop('publisher', None)
+        category_data = validated_data.pop('category', [])
+
+        if author_data:
+            author, created = Author.objects.get_or_create(**author_data)
+            instance.author = author
+
+        if publisher_data:
+            publisher, created = Publisher.objects.get_or_create(**publisher_data)
+            instance.publisher = publisher
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        instance.category.clear()  
+        for category in category_data:
+            category_obj, created = Category.objects.get_or_create(**category)
+            instance.category.add(category_obj)
+
+        return instance
+
+    class Meta:
+        model = Book
+        fields = [
+            "id",
+            "title",
+            "isbn",
+            "pages",
+            "publish_year",
+            "stock",
+            "available_stock",
+            "shelf",
+            "category",
+            "author",
+            "publisher",
+        ]
