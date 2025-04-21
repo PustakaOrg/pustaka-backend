d40cefd31f8d50c2c96db792757c95185a420a5c
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-04-21 19:46:55 +0800

    initial user CRUD

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from apps.authentication.views import UserViewset
+
+router = DefaultRouter()
+router.register(r"users", UserViewset)
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("login/refresh/", TokenRefreshView.as_view(), name="login_refresh"),
 ]
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from apps.authentication.models import User
+from apps.authentication.serializers import UserSerializer
+from core.permissions import IsAdmin
+
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdmin]
