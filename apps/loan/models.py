756c5473fa5d306a3bc4aed515a3246063d8bb0a
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-05-11 13:05:14 +0800

    When updateing status to LOST add a fine accordingly

--- apps/loan/models.py
+++ apps/loan/models.py
@@ -3,16 +3,17 @@ from django.db import models
 
 from apps.catalog.models import Book
 from apps.profiles.models import Librarian, Member
+from apps.settings.models import Settings
 from core.models import BaseModel
 
-# TODO: When updateing status to LOST add a fine accordingly
+
 class Loan(BaseModel):
     STATUS_CHOICES = [
         ("active", "Active"),
         ("returned", "Returned"),
         ("overdue", "Overdue"),
         ("lost", "Lost"),
-        ("done", "Done")
+        ("done", "Done"),
     ]
     loan_date = models.DateTimeField()
     return_date = models.DateTimeField()
@@ -21,7 +22,11 @@ class Loan(BaseModel):
     )
     book = models.ForeignKey(to=Book, on_delete=models.CASCADE, related_name="loans")
     approved_by = models.ForeignKey(
-        to=Librarian, on_delete=models.DO_NOTHING, related_name="approved_loans", null=True, blank=True
+        to=Librarian,
+        on_delete=models.DO_NOTHING,
+        related_name="approved_loans",
+        null=True,
+        blank=True,
     )
     return_procced_by = models.ForeignKey(
         to=Librarian,
@@ -36,6 +41,16 @@ class Loan(BaseModel):
         if self.return_date < self.loan_date:
             raise ValidationError("Return date must be after load date.")
 
+    def save(self, *args, **kwargs):
+        if self.pk is not None:
+            original = Loan.objects.get(pk=self.pk)
+            if original.status != "lost" and self.status == "lost":
+                payment = Payment()
+                fine_ammount = Settings.objects.get_instance().fine_for_lost
+                fine = Fine(amount=fine_ammount, loan=self, payment=payment)
+                fine.save()
+        super().save(*args, **kwargs)  # Call the original save method
+
 
 # TODO: When updating payment status to DOne update its Loan status to Done
 class Payment(BaseModel):
@@ -44,7 +59,11 @@ class Payment(BaseModel):
         ("done", "Done"),
     ]
     accepted_by = models.ForeignKey(
-        to=Librarian, on_delete=models.DO_NOTHING, related_name="payments"
+        to=Librarian,
+        on_delete=models.DO_NOTHING,
+        related_name="payments",
+        null=True,
+        blank=True,
     )
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
 
