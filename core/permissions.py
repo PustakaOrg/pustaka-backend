e6e564defa97228fc92d6356acdf4c4448b0fe8b
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-07-02 20:18:40 +0800

    fix: public not get books

--- core/permissions.py
+++ core/permissions.py
@@ -3,8 +3,6 @@ from rest_framework import permissions
 
 class IsAdminOrLibrarianModify(permissions.BasePermission):
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
         if request.method in permissions.SAFE_METHODS:
             return True
 
